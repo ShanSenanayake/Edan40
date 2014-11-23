ab5d374f5d75a76cb2056867f0ca31356de3aecb
ShanSenanayake <senanayake92@gmail.com>
2014-11-23 18:21:25 +0100

    name Change

--- Haskore2010/autocomp.hs
+++ Haskore2010/autocomp.hs
@@ -9,8 +9,8 @@ type ChordProgression = [(Chord,Dur)]
 
 bassLine :: BassStyle -> [NoteAttribute]->[Pitch]-> [Music]
 bassLine Basic vol = basicBassLine vol 0
-bassLine Calypso vol = calypsoBassLine vol 0
-bassLine Boogie vol = boogieBassLine vol (-1)
+bassLine Calypso vol = calypsoBassLine vol (-1)
+bassLine Boogie vol = boogieBassLine vol 0
 
 
 basicBassLine ::  [NoteAttribute]-> Int-> [Pitch] -> [Music]
@@ -19,16 +19,16 @@ basicBassLine vol 4 m = (Note  (m!!4) hn vol):(basicBassLine vol 0 m)
 basicBassLine vol _ m = []
 
 
-boogieBassLine ::  [NoteAttribute]-> Int-> [Pitch] -> [Music]
-boogieBassLine vol (-1) m = (hnr):(boogieBassLine vol 0 m)
-boogieBassLine vol 0 m = (Note (m!!0) qn vol):(boogieBassLine vol 2 m)
-boogieBassLine vol 2 m = (Note (m!!2) qn vol):(boogieBassLine vol (-1) m)
-boogieBassLine vol _ m = []
-
-
 calypsoBassLine ::  [NoteAttribute]-> Int-> [Pitch] -> [Music]
-calypsoBassLine vol 0 m = (Note (m!!0) qn vol):(Note (m!!4) qn vol):(calypsoBassLine vol 5 m)
-calypsoBassLine vol 5 m = (Note (m!!5) qn vol):(Note (m!!4) qn vol):(calypsoBassLine vol 0 m)
+calypsoBassLine vol (-1) m = (hnr):(boogieBassLine vol 0 m)
+calypsoBassLine vol 0 m = (Note (m!!0) qn vol):(boogieBassLine vol 2 m)
+calypsoBassLine vol 2 m = (Note (m!!2) qn vol):(boogieBassLine vol (-1) m)
 calypsoBassLine vol _ m = []
+
+
+boogieBassLine ::  [NoteAttribute]-> Int-> [Pitch] -> [Music]
+boogieBassLine vol 0 m = (Note (m!!0) qn vol):(Note (m!!4) qn vol):(calypsoBassLine vol 5 m)
+boogieBassLine vol 5 m = (Note (m!!5) qn vol):(Note (m!!4) qn vol):(calypsoBassLine vol 0 m)
+boogieBassLine vol _ m = []
 
 
