189a125f20e583c21aa6444be623ebd094d07ac6
ShanSenanayake <senanayake92@gmail.com>
2014-11-24 17:29:17 +0100

    More chords!

--- Haskore2010/autocomp.hs
+++ Haskore2010/autocomp.hs
@@ -7,6 +7,7 @@ type Chord = (Int,Int,Int)
 type ChordProgression = [(PitchClass,Dur)]
 
 majorScale = [0,2,4,5,7,9,11]
+chordRange = [(E,4),(F,4),(G,4),(A,4),(B,4),(C,5),(D,5),(E,5),(F,5),(G,5)]
 
 generatePitchScale :: Key -> Octave -> PitchClass -> [Pitch]
 generatePitchScale key octave start = map pitch (map ((12*octave + key)+) (shift (abs ((pitchClass start) - key)) majorScale))
@@ -17,34 +18,34 @@ shift n list@(x:xs)
 	| n == x = list
 	| otherwise = shift n (xs++[12+x])
 
-bassLine :: BassStyle -> [NoteAttribute]->Dur->[Pitch]-> Music
-bassLine Basic vol dur p = line  (take (ceiling(2*(rtof dur)))  (basicBassLine vol 0 p))
-bassLine Calypso vol dur p = line  (take (ceiling(8*(rtof dur)))  (calypsoBassLine vol (-1) p))
-bassLine Boogie vol dur p =line  (take (ceiling(8*(rtof dur)))  (boogieBassLine vol 0 p))
+bassLine :: BassStyle ->Dur -> [NoteAttribute]->[Pitch]-> Music
+bassLine Basic dur vol = line . take (ceiling  (2*  (rtof dur))) . basicBassLine 0 vol
+bassLine Calypso dur vol = line . take (ceiling(8*(rtof dur))) . calypsoBassLine (-1) vol
+bassLine Boogie dur vol =line . take (ceiling(8*(rtof dur))) . boogieBassLine 0 vol
 
 
-basicBassLine ::  [NoteAttribute]-> Int-> [Pitch] -> [Music]
-basicBassLine vol 0 m = (Note  (m!!0) hn vol):(basicBassLine vol 4 m)
-basicBassLine vol 4 m = (Note  (m!!4) hn vol):(basicBassLine vol 0 m)
-basicBassLine vol _ m = []
+basicBassLine :: Int->  [NoteAttribute]-> [Pitch] -> [Music]
+basicBassLine 0 vol m = (Note  (m!!0) hn vol):(basicBassLine 4 vol m)
+basicBassLine 4 vol m = (Note  (m!!4) hn vol):(basicBassLine 0 vol m)
+basicBassLine _ vol m = []
 
 
-calypsoBassLine ::  [NoteAttribute]-> Int-> [Pitch] -> [Music]
-calypsoBassLine vol (-1) m = (enr):(enr):(calypsoBassLine vol 0 m)
-calypsoBassLine vol 0 m = (Note (m!!0) en vol):(calypsoBassLine vol 2 m)
-calypsoBassLine vol 2 m = (Note (m!!2) en vol):(calypsoBassLine vol (-1) m)
-calypsoBassLine vol _ m = []
+calypsoBassLine ::  Int-> [NoteAttribute]-> [Pitch] -> [Music]
+calypsoBassLine (-1) vol m = (enr):(enr):(calypsoBassLine 0 vol m)
+calypsoBassLine 0 vol m = (Note (m!!0) en vol):(calypsoBassLine 2 vol m)
+calypsoBassLine 2 vol m = (Note (m!!2) en vol):(calypsoBassLine (-1) vol m)
+calypsoBassLine _ vol m = []
 
 
-boogieBassLine ::  [NoteAttribute]-> Int-> [Pitch] -> [Music]
-boogieBassLine vol 0 m = (Note (m!!0) en vol):(Note (m!!4) en vol):(boogieBassLine vol 5 m)
-boogieBassLine vol 5 m = (Note (m!!5) en vol):(Note (m!!4) en vol):(boogieBassLine vol 0 m)
-boogieBassLine vol _ m = []
+boogieBassLine :: Int->  [NoteAttribute]-> [Pitch] -> [Music]
+boogieBassLine 0 vol m = (Note (m!!0) en vol):(Note (m!!4) en vol):(boogieBassLine 5 vol m)
+boogieBassLine 5 vol m = (Note (m!!5) en vol):(Note (m!!4) en vol):(boogieBassLine 0 vol m)
+boogieBassLine _ vol m = []
 
 
 autoBass :: BassStyle -> Key -> ChordProgression -> Music
-autoBass style key [(c,d)] = (bassLine style [Volume 80] d (generatePitchScale key 3 c))
-autoBass style key ((c,d):prog) = (bassLine style [Volume 80] d (generatePitchScale key 3 c)):+:(autoBass style key prog)
+autoBass style key [(c,d)] = (bassLine style d [Volume 80] (generatePitchScale key 3 c))
+autoBass style key ((c,d):prog) = (bassLine style d [Volume 80] (generatePitchScale key 3 c)):+:(autoBass style key prog)
 
 
 --autoChord :: Key -> ChordProgression -> Music
@@ -52,3 +53,6 @@ autoBass style key ((c,d):prog) = (bassLine style [Volume 80] d (generatePitchSc
 getChord :: Key -> PitchClass -> Chord
 getChord key pitch= (pitchClass (fst (scale!!0)),pitchClass (fst (scale!!2)),pitchClass (fst (scale!!4)))
 	where scale = generatePitchScale key 4 pitch
+
+
+--optimiseChord :: Chord -> Chord -> Music
