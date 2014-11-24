3e2adbe43a81519497a42cbb10f9325d5e58d795
dat11fp1 <dat11fp1@student.lu.se>
2014-11-24 11:19:52 +0100

    Twinkle

---
+++ Haskore2010/twinkle.hs
@@ -0,0 +1,29 @@
+ module Twinkle where
+ import Haskore
+ import AutoComp
+ 
+ -- note updaters for mappings
+ fd d n = n d v
+ vol  n = n   v
+ v      = [Volume 80]
+ lmap f l = line (map f l)
+
+ -- repeat something n times
+ times 1 m = m
+ times n m = m :+: (times (n - 1) m)
+
+
+
+
+ 
+ -- Main Voice:
+ v1a = lmap (fd qn) [c 5, c 5, g 5, g 5, a 5, a 5] :+: g 5 hn v :+: lmap (fd qn) [f 5, f 5, e 5 , e 5, d 5, d 5] :+: c 5 qn v 
+ v1b = lmap (fd qn)    [g 5, g 5, f 5, f 5, e 5, e 5] :+: d 5 hn v
+
+ mainVoice = v1a :+: (times 2 v1b) :+: v1a
+
+-- Putting it all together:
+ twinkle = Instr "piano" (Tempo 2 (mainVoice))
+ progression1 = [(C,wn),(F,hn),(C,hn),(G,hn),(C,hn),(G,hn),(C,hn)]
+ progression2 = [(C,hn),(G,hn),(C,hn),(G,hn),(C,hn),(G,hn),(C,hn),(G,hn)]
+ progression = progression1 ++ progression2 ++ progression1
